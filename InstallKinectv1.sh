5dfae2719b1808b92bd7c3a02b168dc435c0b6d5
Luiz Fernando Pedroso <luizfepe@gmail.com>
2020-12-26 08:46:16 -0300

    Update InstallKinectv1.sh

--- InstallKinectv1.sh
+++ InstallKinectv1.sh
@@ -1,8 +1,11 @@
 
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get git-all
 sudo apt-get install -y cmake libudev0 libudev-dev freeglut3 freeglut3-dev libxmu6 libxmu-dev libxi6 libxi-dev
 sudo apt-get install -y git build-essential libusb-1.0-0-dev
+
+# only if you are building the examples:
+sudo apt-get install freeglut3-dev libxmu-dev libxi-dev
 
 git clone https://github.com/OpenKinect/libfreenect
 cd libfreenect
